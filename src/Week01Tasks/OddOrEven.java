5ee4a287c0eaef7a4486af9604901012b6b06c70
donnellreuben <dreuben@dinklabs.io>
2024-11-14 10:31:26 -0600

    Added Task

--- src/Week01Tasks/OddOrEven.java
+++ src/Week01Tasks/OddOrEven.java
@@ -1,2 +1,16 @@
-package Week01Tasks;public class OddOrEven {
+package Week01Tasks;
+
+public class OddOrEven {
+
+    /*
+    Write a method which can identifies given number is even or odd
+
+        EX:
+        identify(5) ->  "Odd"
+        identify(6) ->  "Even"
+
+     */
+
+
+
 }
