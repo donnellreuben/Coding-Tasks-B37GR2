3e9d716b188558636834b1bee47d65b45572d007
lejlamurselovic <l.murselovic@gmail.com>
2024-11-14 12:34:12 -0500

    Lejla's solution.

--- src/Week01Tasks/OddOrEven.java
+++ src/Week01Tasks/OddOrEven.java
@@ -12,8 +12,28 @@ public class OddOrEven {
      */
 
 
+        public static void main(String[] args) {
 
 
+            System.out.println(identify(5));
+            System.out.println(identify(6));
+        }
 
 
+    public static String identify(int number) {
+        if (number % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
