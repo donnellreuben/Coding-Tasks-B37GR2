b3ef7c3cf0b5536fc1856720e9819514275fb7e7
TroyYC <troy@cydeo.com>
2024-11-21 12:14:17 -0500

    week01 solutions added

--- src/troyWeeklyTasks/Week01/OddOrEven.java
+++ src/troyWeeklyTasks/Week01/OddOrEven.java
@@ -1,5 +1,7 @@
 package troyWeeklyTasks.Week01;
 
+import java.util.Scanner;
+
 public class OddOrEven {
 
       /*
@@ -12,5 +14,55 @@ public class OddOrEven {
         Mykola is volunteer for this
 
      */
+
+    public  static  String  identify( int  n ) {
+
+        return  n % 2==0 ? "Even" : "odd";
+
+    }
+
+    //-----------------------------------------------------------------------
+
+    public static void identify1(){
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter a number to see if it is odd or even: ");
+        int num = reader.nextInt();
+
+        if(num % 2 == 0)
+            System.out.println(num + " is even");
+        else
+            System.out.println(num + " is odd");
+    }
+
+    //----------------------------------------------------------------------------------------
+
+    public static void identify2(){
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = reader.nextInt();
+
+        String evenOdd = (num % 2 == 0) ? "even" : "odd";
+
+        System.out.println(num + " is " + evenOdd);
+    }
+
+    //-------------------------------------------------------------------------
+
+    public static void main(String[] args) {
+        //System.out.println(identify(5));
+
+        //identify(5);
+
+
+
+
+        identify1();
+
+
+    }
+
 
 }
