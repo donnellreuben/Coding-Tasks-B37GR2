f669238b3e6f3ee91f4b862eef7fca5fb607bfa6
Donnell Reuben <117698398+donnellreuben@users.noreply.github.com>
2024-11-25 12:19:24 -0600

    Update FinraTask.java
    cmd + shft+ L it

--- src/donnellWeeklyTasks/Week02/FinraTask.java
+++ src/donnellWeeklyTasks/Week02/FinraTask.java
@@ -13,13 +13,13 @@ public class FinraTask {
         finRa();
     }
 
-    public static void finRa(){
+    public static void finRa() {
 
-        for(int i = 1; i <= 30; i++) {
+        for (int i = 1; i <= 30; i++) {
 
-            if(i % 3 == 0 && i % 5 == 0) {
+            if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FINRA");
-            } else if(i % 3 == 0 ){
+            } else if (i % 3 == 0) {
                 System.out.println("FIN");
             } else if (i % 5 == 0) {
                 System.out.println("RA");
