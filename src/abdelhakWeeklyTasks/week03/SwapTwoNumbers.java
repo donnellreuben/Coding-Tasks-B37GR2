b925deb9eee03c8725cc2b8bb113c2f2b60e18b3
ABDELHAKABDANE <abdelhakabdane7@gmail.com>
2024-12-03 15:21:14 -0600

    Added week03 task

---
+++ src/abdelhakWeeklyTasks/week03/SwapTwoNumbers.java
@@ -0,0 +1,20 @@
+package abdelhakWeeklyTasks.week03;
+
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+
+            int num1 = 9;
+            int num2 = 4;
+
+            num1 = num1 + num2;
+            num2 = num1 - num2;
+            num1 = num1 - num2;
+
+            System.out.println(" num1 = " + num1 + " \n num2 = " + num2);
+
+        }
+    }
+        
+        
+
+
