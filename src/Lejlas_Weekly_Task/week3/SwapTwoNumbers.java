77608064771d8fd3d2d3b05dc659a17ed83c7fd4
lejlamurselovic <l.murselovic@gmail.com>
2024-12-03 12:28:04 +0100

    Swap Two Numbers-Lejla

---
+++ src/Lejlas_Weekly_Task/week3/SwapTwoNumbers.java
@@ -0,0 +1,48 @@
+package Lejlas_Weekly_Task.week3;
+
+
+
+    public class SwapTwoNumbers {
+     /*
+
+
+    Swap two variable values without using a third variable
+
+    int a = 5;
+    int b = 7;
+
+   System.out.println("a = " + a); //=> a = 7
+   System.out.println("b = " + b); //=> b = 5
+
+
+
+
+
+      Ramina is volunteer for this
+
+     */
+
+        public static void main(String[] args) {
+
+            int a = 5;
+            int b = 7;
+
+            a = a * b;
+            b = a / b;
+            a = a / b; 
+
+
+
+            System.out.println("a = " + a); //=> a = 7
+            System.out.println("b = " + b); //=> b = 5
+
+
+        }
+
+
+    }
+
+
+
+
+
