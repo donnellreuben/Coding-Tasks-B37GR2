d9f42a444e3c8ae6ba7ee159eff1e8d49fb8144b
lejlamurselovic <l.murselovic@gmail.com>
2024-12-09 11:08:28 +0100

    Number Reverse Negative-Lejla

---
+++ src/Lejlas_Weekly_Task/week4/NumberReverseNegative.java
@@ -0,0 +1,29 @@
+package Lejlas_Weekly_Task.week4;
+
+public class NumberReverseNegative {
+            /*
+    Write a return method that can reverse
+negative number and return it as int
+
+      Ex:
+      input => -456
+      output => -654
+        Halnur is volunteered
+     */
+
+    public static void main(String[] args) {
+
+
+        int input = -456;
+
+        String inputString = Integer.toString(input);
+
+        StringBuilder reverse = new StringBuilder(inputString.substring(1));
+        System.out.print("-");
+        for (int i = reverse.length() - 1; i >= 0; i--) {
+            System.out.print(reverse.charAt(i));
+        }
+    }
+}
+
+
