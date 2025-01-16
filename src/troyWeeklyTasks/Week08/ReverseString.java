2758086af0fcfa19897600876095046792ca59e1
TroyYC <troy@cydeo.com>
2025-01-16 14:56:28 -0500

    week08 task added and also week7 solution

---
+++ src/troyWeeklyTasks/Week08/ReverseString.java
@@ -0,0 +1,15 @@
+package troyWeeklyTasks.Week08;
+
+public class ReverseString {
+
+
+     /*
+    Write a function that can reverse a String
+        Ex: Reverse("ABCD"); ==> DCBA
+     */
+
+    //Lejla is volunteer
+
+
+
+}
