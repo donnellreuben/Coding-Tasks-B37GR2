095e1163749ad06158baf3cfdb08ea5f47856e01
Nick <mhodynchuk@gmail.com>
2025-02-05 18:37:46 -0600

    week07 FindUniqueChars

---
+++ src/nickWeeklyTasks/Week07/FindUniqueChars.java
@@ -0,0 +1,42 @@
+package nickWeeklyTasks.Week07;
+
+public class FindUniqueChars {
+
+     /*
+    Write a return method that can find the unique characters from the String
+            Ex: unique("AAABBBCCCDEF") ==> "DEF";
+
+     */
+
+    //Ramina is volunteer
+
+
+    public static String uniqueChar(String str) {
+        String[] arr = str.split("");
+        String unique1 = "";
+
+        for (int j = 0; j < arr.length; j++) { // AAABBBCCCDEF
+            int num = 0;
+            for (int i = 0; i < arr.length; i++) { // AAABBBCCCDEF
+                if (arr[i].equals(arr[j])) {
+                    num++;
+                }
+            }
+            if (num == 1) { // if it is unique
+                unique1 += arr[j];
+            }
+        }
+
+        return unique1;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(uniqueChar("AAABBBCCCDEF"));
+    }
+
+
+
+
+}
+
